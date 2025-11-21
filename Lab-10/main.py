122b2d1c49f030b21d253bffccf611c14c4b64cb
Sebastian Bury <sebastianbury2004@gmail.com>
2025-11-21 17:17:24 +0000

    added files

---
+++ Lab-10/main.py
@@ -0,0 +1,9 @@
+# Main script
+
+'''
+FedAvg aggregates model weights without sharing data.
+• Works well with small datasets like MNIST.
+• Can be extended to real-world FL scenarios using Flower.
+• No GPU required for this lab. Why?
+'''
+
